158abb86f6b46b5d6995d1433c5a8712e466b45e
Luis Martinez <luismartinezwebdev@gmail.com>
2024-07-21 08:51:02 +0700

    feat: render a list of restaurants

--- features/restaurants/components/list.tsx
+++ features/restaurants/components/list.tsx
@@ -1,9 +1,46 @@
+import { Card, Text, Group, Badge, Container, SimpleGrid } from '@mantine/core';
+
 import { getAllRestaurants } from "../api";
+import { Restaurant } from "../types";
 
 export async function List() {
   const restaurants = await getAllRestaurants()
 
   return (
-    <pre>{JSON.stringify(restaurants, null, 2)}</pre>
+    <RestaurantList restaurants={restaurants} />
   )
 }
+
+const RestaurantCard = ({ restaurant }:{
+  restaurant: Restaurant
+}) => (
+  <Card shadow="sm" padding="lg" radius="md" withBorder>
+    <Group mt="md" mb="xs">
+      <Text fw={500}>{restaurant.name}</Text>
+      <Badge color="pink" variant="light">
+        {restaurant.cuisine_type}
+      </Badge>
+    </Group>
+
+    <Text size="sm" c="dimmed">
+      Location: {restaurant.location}
+    </Text>
+    <Text size="sm" c="dimmed">
+      Seating Capacity: {restaurant.seating_capacity}
+    </Text>
+  </Card>
+);
+
+const RestaurantList = ({
+  restaurants
+}: {
+  restaurants: Restaurant[]
+}) => (
+  <Container>
+    <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
+      {restaurants.map((restaurant) => (
+        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+      ))}
+    </SimpleGrid>
+  </Container>
+);
