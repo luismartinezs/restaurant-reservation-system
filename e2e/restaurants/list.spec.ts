f34a415bac9bbaacee4f6ef0611f45da52d594bd
Luis Martinez <luismartinezwebdev@gmail.com>
2024-08-08 11:17:02 +0700

    feat: past e2e test

--- e2e/restaurants/list.spec.ts
+++ e2e/restaurants/list.spec.ts
@@ -1,10 +1,12 @@
-import { sleep } from '@/common/utils';
 import { test, expect } from '@playwright/test';
 
-test('restaurants listing filters', async ({page}) => {
+test('restaurants listing filters update search params correctly', async ({page}) => {
   await page.goto('/restaurants');
   await page.getByLabel('Italian').check();
   await page.getByLabel('Tokyo').check();
-  await sleep(1000);
-  expect(page.url()).toContain('cuisine=Italian&location=Tokyo');
+  await page.waitForURL(/cuisine=Italian.*location=Tokyo|location=Tokyo.*cuisine=Italian/);
+
+  expect(page.url()).toMatch(/cuisine=Italian/);
+  expect(page.url()).toMatch(/location=Tokyo/);
+  expect(page.url()).not.toMatch(/cuisine=Japanese/);
 });
