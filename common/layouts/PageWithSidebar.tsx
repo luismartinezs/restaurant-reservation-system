f487d56017a5be49162a2fe4773117a5b56ec2b1
Luis Martinez <luismartinezwebdev@gmail.com>
2024-08-14 10:34:03 +0700

    feat: responsive layout

--- common/layouts/PageWithSidebar.tsx
+++ common/layouts/PageWithSidebar.tsx
@@ -13,9 +13,9 @@ export const PageWithSidebar = ({
 }) => {
   return (
     <Container size="lg">
-      <article className="flex flex-col md:flex-row items-start justify-start gap-6">
-        <div className="md:w-2/3 order-2 md:order-1">{children}</div>
-        <aside className="md:w-1/3 order-1 md:order-2">
+      <article className="flex flex-col lg:flex-row items-start justify-start lg:gap-8">
+        <div className="lg:w-2/3 order-2 lg:order-1">{children}</div>
+        <aside className="lg:w-1/3 order-1 lg:order-2">
           {sidebar?.map((nestedBlok) => (
             <StoryblokComponent
               blok={nestedBlok}
